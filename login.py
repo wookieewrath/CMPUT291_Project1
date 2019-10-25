94f6471a1db406f1dd20922e58b0cb558c041d4f
kamillahhasham <37892254+kamillahhasham@users.noreply.github.com>
2019-10-25 12:35:55 -0600

    Add files via upload
    These are the rough implementations of the login functionality of our database. It is working. I just need another half another to make it fully functional. Right now it will properly conclude whether there are users in the table if BOTH the username and password are entered correctly. However if someone makes a mistake and one of the details is misspelled it will just say the user doesn't exists and tell you to create one. I know how to fix it and will work on it when we meet up.

---
+++ login.py
@@ -0,0 +1,51 @@
+import sqlite3
+from User_Class import User
+
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+#Right now this function will find a correct enter of username and password in the table and return that user
+#If it doesn't find the username/password combination in the table it will conclude that the user doesn't exist
+#PROBLEM: What if user has correct username but enters the wrong password by accident??
+#SOLUTION: Implement the following 3 cases:
+#1. The username and password were both found in the table -- return the user's type details
+#2. The username is in the table but the password is wrong -- return True to symbolize you need to reprompt for password
+#3. The username and password are not in the table -- return False to symbolize you need to create a new user
+def does_user_exist(username, password):
+    c.execute("SELECT uid, pwd, utype FROM users WHERE uid = ? AND pwd = ?", (username, password))
+    user_in_table = c.fetchone()
+    if user_in_table == None:
+        return False
+    else:
+        return user_in_table
+
+
+def create_user(uid, pwd, utype, fname, lname, city):
+    c.execute("CREATE TABLE IF NOT EXISTS users(uid TEXT, pwd TEXT, utype CHAR(1), fname TEXT, lname TEXT, city TEXT)")
+    
+    if does_user_exist(uid, pwd) == False:
+        user = User(uid, pwd, utype, fname, lname, city)
+        c.execute("INSERT INTO users VALUES(?,?,?,?,?,?)", (user.uid, user.pwd, user.utype, user.fname, user.lname, user.city))
+        conn.commit()
+        
+    else:
+        print("This user already exists inside the database!!")
+
+create_user('Kim', 'Kim123', 'a', 'Kamillah', 'Hasham', 'Edmonton')
+
+
+#You need to adjust this function because when the user puts in a wrong password... it concludes that the user doesn't exist
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+    existing_user = does_user_exist(username, password) 
+    print(existing_user)
+    
+    if existing_user == False:
+        print("This user does NOT exist... please create a user")
+    
+    else:
+        print("This user DOES exist... her type is:" + existing_user[2])
+        
+login()
+    
